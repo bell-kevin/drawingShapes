5496e90a8268364f603a200d66d4abaed22948d0
Kevin Bell <bellKevin@pm.me>
2022-06-16 15:37:31 +0000

    Add files via upload

---
+++ Ch7Draw/src/ch7draw/Ch7Draw.java
@@ -0,0 +1,14 @@
+package ch7draw;
+
+public class Ch7Draw {
+
+    public static void drawBar(int length) {
+        for (int i = 1; i <= length; i++) {
+            System.out.println("*");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+    }
+}
