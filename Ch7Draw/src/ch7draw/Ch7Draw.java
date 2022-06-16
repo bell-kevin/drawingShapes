f3221c2da5b5c66cb5ee202b86de26771c09fec5
Kevin Bell <bellKevin@pm.me>
2022-06-16 18:48:39 +0000

    Update Ch7Draw.java

--- Ch7Draw/src/ch7draw/Ch7Draw.java
+++ Ch7Draw/src/ch7draw/Ch7Draw.java
@@ -4,11 +4,16 @@ import java.util.Scanner;
 
 public class Ch7Draw {
 
-    String characterPreference;
-
     public static void drawBar(int length) {
         for (int i = 1; i <= length; i++) {
             System.out.print("*");
+        }
+        System.out.println();
+    }
+
+    public static void drawBar(int length, String characterPreference) {
+        for (int i = 1; i <= length; i++) {
+            System.out.print(characterPreference);
         }
         System.out.println();
     }
@@ -19,34 +24,37 @@ public class Ch7Draw {
         }
     }
 
+    public static void drawBox(int length, int height, String characterPreference) {
+        for (int i = 1; i <= height; i++) {
+            drawBar(length, characterPreference);
+        }
+    }
+
     public static void main(String[] args) {
         Scanner computerKeyboardInput = new Scanner(System.in);
         System.out.println("Drawing Project by Kevin Bell\n");
-        
-        int width, height;
-        String yesOrNo, characterPreference;
-        
-        //draw a right triangle
-        for (int i = 1; i <= 6; i++) {
-            drawBar(i);
-        }
-        System.out.println();
 
-        //draw a box
-        drawBox(5, 4);
-        System.out.println();
-        System.out.print("How wide is the box? ");
-        width = computerKeyboardInput.nextInt();
-        System.out.print("How tall is the box? ");
-        height = computerKeyboardInput.nextInt();
-        System.out.print("Do you want the default marker (asterisk), Y or N? ");
-        yesOrNo = computerKeyboardInput.next();
-        if (yesOrNo.equalsIgnoreCase("n")) {
-            System.out.print("What character do you want to use? ");
-            characterPreference = computerKeyboardInput.next();
-        } else {
-            characterPreference = "*";
+        int width, height;
+        String yesOrNo, characterPreference, keepPlaying;
+        System.out.print("Do you want me to draw a box? (Y or N) ");
+        keepPlaying = computerKeyboardInput.next();
+        while (keepPlaying.equalsIgnoreCase("y")) {
+            System.out.print("How wide is the box? ");
+            width = computerKeyboardInput.nextInt();
+            System.out.print("How tall is the box? ");
+            height = computerKeyboardInput.nextInt();
+            System.out.print("Do you want the default marker (asterisk), Y or N? ");
+            yesOrNo = computerKeyboardInput.next();
+            if (yesOrNo.equalsIgnoreCase("n")) {
+                System.out.print("What character do you want to use? ");
+                characterPreference = computerKeyboardInput.next();
+            } else {
+                characterPreference = "*";
+            }
+            drawBox(width, height, characterPreference);
+            System.out.println("");
+            System.out.print("Do you want me to draw a box? (Y or N) ");
+            keepPlaying = computerKeyboardInput.next();
         }
-        drawBox(width, height);
     }
 }
